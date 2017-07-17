1f59e0e5500c74319f0670be0c715b203e22983b
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 14:40:03 +0000

    Implemented concern for managing image data

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,11 +9,10 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build }
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
       if @portfolio_item.save
@@ -23,7 +22,7 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def edit
     @portfolio_item = Portfolio.find(params[:id])
   end
@@ -56,4 +55,5 @@ class PortfoliosController < ApplicationController
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
+
 end
