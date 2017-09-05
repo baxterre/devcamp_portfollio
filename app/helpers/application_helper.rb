c35ec1b394d911a993b27ee5a5c8b2bf104e866b
Jordan Hudgens <jordan@devcamp.com>
2017-09-05 14:04:39 +0000

    Integrated styles for blogs pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,6 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+    DevcampViewTool::Renderer.copyright 'Jaxxon Baxter', 'All rights reserved'
   end
 end
